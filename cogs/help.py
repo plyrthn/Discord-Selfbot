plyrthn/Discord-Selfbot

commit c808a5b11deac8f7a05f210a751dbb07c841f065

help cmd fixes
fixed issue with field not getting populated correctly + fix for help cog not working

cogs/help.py
@@ -174,17 +174,17 @@ class Help(formatter.HelpFormatter):
         if isinstance(command, discord.ext.commands.core.Command):
             # <signature portion>
             # emb['embed']['title'] = emb['embed']['description']
-            emb['embed']['description'] = 'Syntax: `{0}`'.format(self.get_command_signature())
+            emb['embed']['description'] = '`Syntax: {0}`'.format(self.get_command_signature())
 
             # <long doc> section
             if command.help:
                 name = '{0}'.format(command.help.split('\n\n')[0])
-                name = name.replace('[p]', self.clean_prefix)
-                name_length = len(name) - 4
+                #name = name.replace('[p]', self.clean_prefix)
+                name_length = len(name)
                 value = command.help[name_length:].replace('[p]', self.clean_prefix)
                 if value == '':
-                     name = '__{0}__'.format(command.help.split('\n')[0])
-                     name_length = len(name) - 4
+                     name = '{0}'.format(command.help.split('\n')[0])
+                     name_length = len(name)
                      value = command.help[name_length:].replace('[p]', self.clean_prefix)
                 if value == '':
                     value = empty

cogs/cog_download.py
@@ -29,6 +29,7 @@ class CogDownloading:
     @commands.group(pass_context=True)
     async def cog(self, ctx):
         """Manage custom cogs from ASCII. [p]help cog for more information.
+        
         The Appu Selfbot Cog Importable Index (aka ASCII) is a server that hosts custom cogs for the bot.
         [p]cog install <cog> - Install a custom cog from ASCII.
         [p]cog uninstall <cog> - Uninstall one of your ASCII cogs.

commit 29ca0fed663b1d2e4cf38567f7cb5f3b2eec5657

fix subtle bug in help text formatting
this addresses a regression introduced in c808a5b.

cogs/help.py
@@ -180,11 +180,11 @@ class Help(formatter.HelpFormatter):
             if command.help:
                 name = '{0}'.format(command.help.split('\n\n')[0])
                 name = name.replace('[p]', self.clean_prefix)
-                name_length = len(name)
+                name_length = len(name) + 4
                 value = command.help[name_length:].replace('[p]', self.clean_prefix)
                 if value == '':
                      name = '{0}'.format(command.help.split('\n')[0])
-                     name_length = len(name)
+                     name_length = len(name) + 2
                      value = command.help[name_length:].replace('[p]', self.clean_prefix)
                 if value == '':
                     value = empty