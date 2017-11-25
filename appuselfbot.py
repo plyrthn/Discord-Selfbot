dc681c40f502fb50845a0af8e2548e22cec64dc5
James <p1n3tr335@gmail.com>
2017-11-25 11:18:23 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-                            temp = context.pop()
                             if temp[0].clean_content:
                                 em.add_field(name='%s' % temp[0].author.name, value=temp[0].clean_content, inline=False)
                         em.add_field(name='%s' % message.author.name, value=message.clean_content, inline=False)
