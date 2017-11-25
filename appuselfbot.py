8d0b5dd7a6b3ef0ef08f1783b513f42ba201c402
James <p1n3tr335@gmail.com>
2017-11-25 11:18:14 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-                            except:
                                 await message.channel.send(response[1])
                         else:
                             await message.channel.send(response[1])
