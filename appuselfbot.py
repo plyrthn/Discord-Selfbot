1b18708c96283471fcb3c15b1b905c69875260d9
James <p1n3tr335@gmail.com>
2017-11-25 11:15:38 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-    parser.add_argument("-test", "--test-run",  # test run flag for Travis
                         action="store_true",
                         help="Makes the bot quit before trying to log in")
     parser.add_argument("--force-mac",  # Allows for Testing of mac related code
