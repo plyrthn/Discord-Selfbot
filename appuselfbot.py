88ccf7099af0e5d881a6220c2ed11379740511a6
James <p1n3tr335@gmail.com>
2017-11-25 11:18:35 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-                                else:
                                     next_avatar += 1
                                 bot.avatar = all_avis[next_avatar]
                                 with open('avatars/%s' % bot.avatar, 'rb') as fp:
