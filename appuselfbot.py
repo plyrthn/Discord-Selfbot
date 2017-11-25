b687cc8254c4fe51323709282097c0c7b0aa037a
James <p1n3tr335@gmail.com>
2017-11-25 11:18:41 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-                print('Failed to load extension {}\n{}: {}'.format(extension, type(e).__name__, e))
     for extension in os.listdir("custom_cogs"):
         if extension.endswith('.py'):
             try:
