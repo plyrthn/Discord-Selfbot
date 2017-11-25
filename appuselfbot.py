2086a4f49b763b63943f74c19e3d00abd7f3f1d0
James <p1n3tr335@gmail.com>
2017-11-25 11:18:25 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-                            logged_msg = '``%s`` mentioned' % word
                         for b, i in enumerate(all_words):
                             if b == 0:
                                 if bot.notify['type'] == 'msg':
