e1a3a38f398156c72fc4183ab36480d86b6b0204
James <p1n3tr335@gmail.com>
2017-11-25 11:18:16 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-        try:
             word_found = False
             if (bot.log_conf['allservers'] == 'True' or str(message.guild.id) in bot.log_conf['servers']) and (str(message.guild.id) not in bot.log_conf['blacklisted_servers'] and str(message.channel.id) not in bot.log_conf['blacklisted_channels']):
                 add_alllog(str(message.channel.id), str(message.guild.id), message)
