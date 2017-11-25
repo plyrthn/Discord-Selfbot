7ed3135cb07066bf1cc57ff2ab77d6b1b46f26f4
James <p1n3tr335@gmail.com>
2017-11-25 11:18:24 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-                                split_msg += b + '\n'
                             all_words.append(split_msg)
                             split_msg = ''
                         if user_found:
