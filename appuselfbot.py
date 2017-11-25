788c1cd003d1b7f8c8c99acd84af8d2c6010a46f
James <p1n3tr335@gmail.com>
2017-11-25 11:18:41 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-                bot.load_extension("custom_cogs." + extension[:-3])
             except Exception as e:
                 print('Failed to load extension {}\n{}: {}'.format(extension, type(e).__name__, e))
 
