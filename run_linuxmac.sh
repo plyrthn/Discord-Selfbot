e87f7462123dad8e28b9ba6ff374d7fe3d2c9395
James <p1n3tr335@gmail.com>
2017-11-25 11:22:02 -0500

    Removed Bloat

--- run_linuxmac.sh
+++ run_linuxmac.sh
@@ -1,4 +1,3 @@
-	else
 		echo "You do not appear to have Python 3 installed"
 		echo "Python 3 is almost certainly available from your package manager or just google how to get it"
 		echo "However if you are, for instance, using Linux from Scratch, you likely do not need instruction"
