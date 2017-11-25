d7a7b63dbc7380c128f7370de72144bf0fc30370
James <p1n3tr335@gmail.com>
2017-11-25 11:18:39 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-            site = requests.get('https://github.com/LyricLy/ASCII/tree/master/cogs').text
             soup = BeautifulSoup(site, "html.parser")
             data = soup.find_all(attrs={"class": "js-navigation-open"})
             list = []
