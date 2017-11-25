6298dd7a6be622aba3e17d49751fae5362fd2431
James <p1n3tr335@gmail.com>
2017-11-25 11:18:21 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-                    is_separate = False
                 guild = bot.get_guild(int(location[1]))
                 if str(message.channel.id) != location[0]:
                     msg = message.clean_content.replace('`', '')
