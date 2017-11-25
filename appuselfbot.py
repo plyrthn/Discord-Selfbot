703aa247ad07f1b1b213b553f1d75ac5c9263189
James <p1n3tr335@gmail.com>
2017-11-25 11:18:31 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-                                    await bot.change_presence(game=discord.Game(name=g, type=1,
                                                                                 url=url),
                                                               status=set_status(bot), afk=True)
                                 else:
