73d09269e66b0450a54e3d257254fde2d7c5916c
James <p1n3tr335@gmail.com>
2017-11-25 11:18:12 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-            if hasattr(bot, 'ignored_servers'):
                 if any(str(message.guild.id) == str(guild_id) for guild_id in bot.ignored_servers['servers']):
                     return
         except AttributeError:  # Happens when it's a direct message.
