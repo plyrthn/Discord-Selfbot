d4959f9828f19d5378b008294386a0d7fefb0cd4
James <p1n3tr335@gmail.com>
2017-11-25 11:15:41 -0500

    Removed Bloat

--- appuselfbot.py
+++ appuselfbot.py
@@ -1,4 +1,3 @@
-                        action="store_true",
                         help="Makes the bot quit before trying to log in")
     parser.add_argument("--force-mac",  # Allows for Testing of mac related code
                         action="store_true",
